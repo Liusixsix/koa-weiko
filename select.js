4783aaac988a615279960229a6b6b7e1ce224d97
liuyan <450995596@qq.com>
2020-08-05 21:50:15 +0800

    查询列表

--- select.js
+++ select.js
@@ -9,7 +9,6 @@ const { Blog,User}  = require('./model')
     })
     // console.log('lisi',lisi.dataValues)
 
-
     // 查询特定的列
     const zhangsanName = await User.findOne({
         attributes:['userName','nickName'],
@@ -17,5 +16,20 @@ const { Blog,User}  = require('./model')
             userName:'李四'
         }
     })
-    console.log('zhangsanName',zhangsanName.dataValues)
+    // console.log('zhangsanName',zhangsanName.dataValues)
+
+
+    // 查询一个列表
+    const zhangsanBlogList = await Blog.findAll({
+        where:{
+            userId:1
+        },
+        order:[
+            ['id','desc'],
+        ]
+    })
+    console.log('zhangsanBlogList',
+    zhangsanBlogList.map(blog=>blog.dataValues)
+    )
+
 })()
