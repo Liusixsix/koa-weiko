62d44d9cdc489fbaf800d9fbaf6e1693281f6793
liuyan <450995596@qq.com>
2020-08-08 11:17:24 +0800

    redis

--- src/cache/_redis.js
+++ src/cache/_redis.js
@@ -2,11 +2,57 @@
  * @description 连接redis的方式 get set
  */
 const redis = require('redis')
- const { REDIS_CONF } = require('../conf/db')
+const { REDIS_CONF } = require('../conf/db')
+const { ne } = require('sequelize/types/lib/operators')
+
 
 
 //  创建客户端
-const redisClient = redis.createClient(REDIS_CONF.port,REDIS_CONF.host)
-redisClient.on('error',err=>{
-    console.error('redisError:',err)
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host)
+redisClient.on('error', err => {
+    console.error('redisError:', err)
 })
+
+/**
+ * 
+ * @param {string} key  key
+ * @param {string} val  val
+ * @param {number} timeout  //过期时间 单位s
+ */
+function set(key, val, timeout = 60 * 60) {
+    if(typeof val === 'object'){
+        val = JSON.stringify(val)
+    }
+    redisClient.set(key,val)
+    redisClient.expire(key,timeout)
+}
+
+/**
+ * 
+ * @param {string} key 键 
+ */
+function get(key){
+    const promise = new Promise((resolve,reject)=>{
+        redisClient.get(key,(err,val)=>{
+            if(err){
+                reject(err)
+                return
+            }
+            if(val==null){
+                resolve(null)
+                return
+            }
+            try{
+                resolve(JSON.parse(val))
+            }catch(e){
+                resolve(val)
+            }
+        })
+    })
+    return promise
+}
+
+
+module.exports ={
+    set,get
+}
--- src/conf/db.js
+++ src/conf/db.js
@@ -1,10 +1,12 @@
 const model = require("../../model")
-
+const { isProd } = require('../utils/env')
 let REDIS_CONF = {
-    port:6379,
-    host:'127.0.0.1'
+    port: 6379,
+    host: '127.0.0.1'
 }
-
+if (isProd) {
+    // 线上配置
+}
 module.exports = {
     REDIS_CONF
 }
